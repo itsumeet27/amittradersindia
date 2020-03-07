ac76a482fe128789fa600182b61b101a88c8bb69
Sumeet Sharma <sksksharma0@gmail.com>
2020-03-07 15:04:37 +0530

    Adding navigation and meta tags

--- includes/header.php
+++ includes/header.php
@@ -4,6 +4,9 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <meta name="description" content="Amit Traders deals with manufacturing and supplying of leather products and corporate gift articles. Our products include Laptop Bags, Passport holders and Gents Wallet.">
+  <meta name="author" content="Sumeet Sharma - Software Engineer | UI/Frontend Developer | Web Developer">
+  <meta name="keywords" content="Genuine leather products online, leather products for corporate gifts, best leather products online, laptop bags in genuine leather, gents wallet in genuine leather, passport holder in genuine leather">
   <title>Amit Traders India - Manufacturers and Suppliers of leather goods</title>
   <!-- MDB icon -->
   <link rel="icon" href="img/logo.ico" type="image/x-icon">
@@ -19,3 +22,40 @@
   <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
+    <!--Navbar -->
+        <nav class="mb-1 navbar navbar-expand-lg navbar-dark default-color elegant-color">
+        <a class="navbar-brand" href="#">Amit Traders</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent-333"
+            aria-controls="navbarSupportedContent-333" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent-333">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="/">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#about">About</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#products">Products</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#contact">Contact</a>
+                </li>
+            </ul>
+            <ul class="navbar-nav ml-auto nav-flex-icons">
+                <li class="nav-item">
+                    <a class="nav-link waves-effect waves-light" href="https://www.facebook.com/amittradersindia">
+                    <i class="fab fa-facebook"></i>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link waves-effect waves-light" href="https://www.instagram.com/amittradersindia">
+                    <i class="fab fa-instagram"></i>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        </nav>
+    <!--/.Navbar -->
